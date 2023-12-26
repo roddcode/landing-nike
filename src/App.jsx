c7433fbc7393feb8d49334de019401240b93f6c7
Alejandro Alvarado <devale.alvarado@gmail.com>
2023-12-25 20:26:59 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import Nav from './components/Nav'
+import {CustomerReviews, Footer, Hero, PopularProducts, Services, SpecialOffer, Subscribe, SuperQuality, } from './sections/index'
+const App = () => {
+  return (
+    <main className='text-black'>
+      <Nav />
+      <section
+        className='xl:padding-1
+    wide:padding-r padding-b'
+      >
+        <Hero />
+      </section>
+      <section className='padding'>
+        <PopularProducts />
+      </section>
+      <section className='padding'>
+        <SuperQuality />
+      </section>
+      <section className='padding-x py-10'>
+        <Services />
+      </section>
+      <section className='padding'>
+        <SpecialOffer />
+      </section>
+      <section className="bg-pale-blue padding">
+        <CustomerReviews />
+      </section>
+      <section className="padding-x sm:py-32 py-16 w-full">
+        <Subscribe />
+      </section>
+      <section className="padding-x padding-t pb-8 bg-black ">
+        <Footer />
+      </section>
+    </main>
+  )
+}
+export default App
---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from './Hero'
+import PopularProducts from './PopularProducts'
+import SuperQuality from './SuperQuality'
+import Footer from './Footer'
+import Subscribe from './Subscribe'
+import Services from './Services'
+import SpecialOffer from './SpecialOffer'
+import CustomerReviews from './CustomerReviews'
+
+export {
+  Hero,
+  SuperQuality,
+  PopularProducts,
+  Footer,
+  Subscribe,
+  Services,
+  SpecialOffer,
+  CustomerReviews,
+}

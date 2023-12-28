8573131e39b203f9320d493c2dc8b9ff31041d19
Alejandro Alvarado <devale.alvarado@gmail.com>
2023-12-28 17:45:31 -0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import Nav from './components/Nav'
-import {CustomerReviews, Footer, Hero, PopularProducts, Services, SpecialOffer, Subscribe, SuperQuality, } from './sections/index'
+import {
+  CustomerReviews,
+  Footer,
+  Hero,
+  PopularProducts,
+  Services,
+  SpecialOffer,
+  Subscribe,
+  SuperQuality,
+} from './sections/index'
 const App = () => {
   return (
-    <main className='text-black'>
+    <main className='relative'>
       <Nav />
-      <section
-        className='xl:padding-1
-    wide:padding-r padding-b'
-      >
+      <section className='xl:padding-l wide:padding-r padding-b'>
         <Hero />
       </section>
       <section className='padding'>
@@ -22,13 +28,13 @@ const App = () => {
       <section className='padding'>
         <SpecialOffer />
       </section>
-      <section className="bg-pale-blue padding">
+      <section className='bg-pale-blue padding'>
         <CustomerReviews />
       </section>
-      <section className="padding-x sm:py-32 py-16 w-full">
+      <section className='padding-x sm:py-32 py-16 w-full'>
         <Subscribe />
       </section>
-      <section className="padding-x padding-t pb-8 bg-black ">
+      <section className='padding-x padding-t pb-8 bg-black '>
         <Footer />
       </section>
     </main>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,3 +1,4 @@
+import { hamburger } from '../assets/icons'
 import {headerLogo} from '../assets/images'
 import {navLinks} from '../constants'
 const Nav = () => {
@@ -10,12 +11,20 @@ const Nav = () => {
         <ul className='flex-1 flex justify-center items-center gap-16 max-lg:hidden'>
           {navLinks.map((item) => (
             <li key={item.label}>
-              <a href={item.href} className='font-monts errat leading-normal text-lg text-slate-gray'>
+              <a href={item.href} className='font-monts errat leading-normal text-lg text-slate-gray hover:text-slate-700'>
                 {item.label}
               </a>
             </li>
           ))}
         </ul>
+        <div className='flex gap-2 text-lg leading-normal font-medium font-montserrat max-lg:hidden wide:mr-24'>
+          <a href="/">Sign in</a>
+          <span>/</span>
+          <a href="/">Explore now</a>
+        </div>
+        <div className='hidden max-lg:block'>
+          <img src={hamburger} alt="Hamburger" width={25} height={25} />
+        </div>
       </nav>
     </header>
   )
